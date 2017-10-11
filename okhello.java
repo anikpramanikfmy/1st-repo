8e94a5bba258f04fc0a80d9e8451af4b060c61c3
anikpramanikfmy <anikpramanik.fm.y@gmail.com>
2017-10-11 22:55:17 +0600

    java hello world

---
+++ okhello.java
@@ -0,0 +1,12 @@
+package okhello;
+
+
+
+public class okhello {
+	
+
+	 public static void main( String[] args ) {
+	        System.out.println( "Hello World!" );
+	       System.out.println("hi");
+	 }
+}
